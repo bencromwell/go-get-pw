258cb0cdbf1b6c9fab002333cda314acc6daffee
Ben Cromwell <ben@bencromwell.com>
2021-03-16 17:09:46 +0000

    Extract method

--- pwextract/pwextract.go
+++ pwextract/pwextract.go
@@ -4,10 +4,14 @@ func SelectedCharactersFromPassword(password string, characters []int) map[int]s
 	selected := make(map[int]string)
 
 	for _, index := range characters {
-		if index >= 1 && len(password) > index-1 {
+		if indexIsValid(password, index) {
 			selected[index] = string([]rune(password)[index-1])
 		}
 	}
 
 	return selected
+}
+
+func indexIsValid(password string, index int) bool {
+	return index >= 1 && len(password) > index-1
 }
